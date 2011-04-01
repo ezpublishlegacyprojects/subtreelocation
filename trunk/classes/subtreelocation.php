c021f76ed22eef5d63e128ae726c6c38d4ef6276
jbouzekri <info@example.com>
2011-04-01 21:21:05 +0000

    Add comment in algo
    git-svn-id: http://svn.projects.ez.no/subtreelocation@4 d24d8dc8-9c09-4451-8b63-177a843adc94

--- trunk/classes/subtreelocation.php
+++ trunk/classes/subtreelocation.php
@@ -4,6 +4,7 @@ class subtreeLocation
 {
     static function addAssignment( $nodeID, $objectID, $selectedNodeIDArray )
     {
+        // Merge all node to place in an array
         $nodeToPlaceArray = array();
         $rootNode = eZContentObjectTreeNode::fetch($nodeID);
         if ( !$rootNode )
@@ -13,14 +14,17 @@ class subtreeLocation
         $rootNodeChildren = $rootNode->subTree();
         $nodeToPlaceArray = array_merge($nodeToPlaceArray, $rootNodeChildren);
         
+        // Operation for each selected node
         foreach ($selectedNodeIDArray as $selectedNodeID)
         {
             $selectedNode = eZContentObjectTreeNode::fetch($selectedNodeID);
             if ( !$selectedNode )
                 return array( 'status' => false );
             
+            // Operation for each node to locate
             foreach ($nodeToPlaceArray as $i => $nodeToPlace)
             {
+                // Manage the root node of the subtree
                 if ($i == 0)
                 {
                     $tmpSelectedNodeIDArray = array($selectedNodeID);
@@ -30,12 +34,15 @@ class subtreeLocation
                 }
                 else
                 {
+                    // Manage the other nodes
                     $parentNodeToPlace = $nodeToPlace->fetchParent();
                     $parentObjectNodeToPlace = $parentNodeToPlace->object();
                     $parentNodeToPlaceAssignmentArray = $parentObjectNodeToPlace->assignedNodes();
                     foreach ($parentNodeToPlaceAssignmentArray as $parentNodeToPlaceAssignment) {
                         $isLocation = strpos($parentNodeToPlaceAssignment->PathString,'/'.$selectedNodeID.'/');
                         $tmpSelectedNodeIDArray = array($parentNodeToPlaceAssignment->NodeID);
+                        // If the parent node is already located and this location contains the selected node
+                        // Add location to the current node to locate
                         if ($isLocation !== false)
                         {
                             $operation = eZContentOperationCollection::addAssignment( $nodeToPlace->NodeID, $nodeToPlace->ContentObjectID, $tmpSelectedNodeIDArray );
