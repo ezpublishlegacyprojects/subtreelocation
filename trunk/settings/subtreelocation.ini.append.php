802e4507b0090c09ef140671fcc76dda9bbceb72
jbouzekri <info@example.com>
2011-04-09 19:28:40 +0000

    Add INSTALL file
    git-svn-id: http://svn.projects.ez.no/subtreelocation@11 d24d8dc8-9c09-4451-8b63-177a843adc94

--- trunk/settings/subtreelocation.ini.append.php
+++ trunk/settings/subtreelocation.ini.append.php
@@ -1,11 +1,12 @@
 <?php /* #?ini charset="utf-8"?
 
 [ScriptBin]
+# the user name used when adding location with the bin/php/subtreelocation.php script
 UserName=admin
 
 [ScriptMonitor]
 # Max size for add location direct without schedule in ezscriptmonitor
-MaxSubtreeSize=2
+# MaxSubtreeSize=2
 
 
 */ ?>
